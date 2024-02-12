db37f8f78e2e98ce29d108c9a7f21ad0309c1e37
kurobei0314 <maikurodei.biseibutu@gmail.com>
2024-02-12 18:31:01 +0900

    [refactor] いらないコメントアウトを削除

--- FarmDefenseGame/Assets/Scripts/Presenter/PlayerDamagePresenter.cs
+++ FarmDefenseGame/Assets/Scripts/Presenter/PlayerDamagePresenter.cs
@@ -50,32 +50,3 @@ namespace WolfVillageBattle
     
     }
 }
-// // 敵からの攻撃を受けた時の挙動
-//         body.OnTriggerEnterAsObservable()
-//             .Subscribe(_ => {
-//                 var parent = _.gameObject.transform.parent;
-//                 if (parent == null || parent.gameObject.tag != "Enemy") return;
-//                 // TODO：これも絶対仕組み変える絶対に
-//                 if (CurrentStatus != PlayerStatus.IDLE) return;
-//                 var enemy_status = parent.gameObject.GetComponent<EnemyTest>().current_enemy_status;
-//                 if (enemy_status != EnemyTest.EnemyStatus.ATTACK) return;
-//                 Debug.Log("敵から攻撃されたおー");
-//                 ReserveDamage(parent.gameObject.GetComponent<EnemyTest>().Attack);
-//                 UpdateHPbar(hp/(float)max_hp);
-//                 if (hp == 0) {
-//                     SetPlayerStatus(PlayerStatus.DIE);
-//                     Debug.Log("HPが0になっちゃったー");
-//                     AudioManager.Instance.PlaySE("unitychan_lose");
-//                     unity_chan.GetComponent<Animator>().Play("GoDown");
-//                     playerDie.OnNext(1);
-//                 }
-//                 else {
-//                     float rnd = UnityEngine.Random.Range(0.0f, 1.0f);
-//                     if (rnd < 0.5f){
-//                         AudioManager.Instance.PlaySE("unitychan_damage1");
-//                     } else{
-//                         AudioManager.Instance.PlaySE("unitychan_damage2");
-//                     }
-//                     StartCoroutine ("PlayAnimDamage");
-//                 }
-//         }).AddTo(this);
