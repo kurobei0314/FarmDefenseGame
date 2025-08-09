e267bbf2eefb9dedcff249e37e2416c91acd983e
kurobei0314 <maikurodei.biseibutu@gmail.com>
2025-08-09 16:40:34 +0900

    [refactor] 一旦初期化処理を親のコントローラーに移す

--- FarmDefenseGame/Assets/Scripts/PlayerMenuUI/View/PlayerMenuUI/PlayerMenuUI.cs
+++ FarmDefenseGame/Assets/Scripts/PlayerMenuUI/View/PlayerMenuUI/PlayerMenuUI.cs
@@ -1,96 +1,34 @@
 using UnityEngine;
-using WolfVillage.MasterDataStore;
-using WolfVillage.Entity;
-using System.Linq;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
-using WolfVillage.Battle;
 using WolfVillage.Search.PlayerMenuUI.EquipmentMenu;
 using WolfVillage.Search.PlayerMenuUI.SkillMenu;
-using System.Collections.Generic;
-using WolfVillage.Interface;
-using WolfVillage.Entity.Interface;
-using System;
 using Extension;
+using WolfVillage.Entity.Interface;
 
 namespace WolfVillage.Search.PlayerMenuUI
 {
-    public class PlayerMenuUI : MonoBehaviour
+    public class PlayerMenu : MonoBehaviour
     {
         [SerializeField] private PlayerMenuHeaderUI _headerUI;
         [SerializeField] private PlayerMenuContentUI _contentUI;
 
-        // TODO: 仮
-        [SerializeField] private WeaponVODataStore weaponVODataStore;
-        [SerializeField] private SkillVODataStore skillVODataStore;
-        [SerializeField] private ArmorVODataStore armorVODataStore;
-        [SerializeField] private PlayerStatusVODataStore playerDataStore;
-        private SearchPlayerEntity player;
-        private WeaponEntity[] _weaponEntities;
-        private ArmorEntity[] _armorEntities;
         private PlayerMenuUIVM _playerMenuVM;
 
-        // TODO: masterやユーザーデータの情報は、ここで取得するわけでなく別のところに持たせるようにする
-        void Start()
+        public void Initialize( IPlayerEntity player,
+                                IWeaponEntity[] weaponEntities,
+                                IArmorEntity[] armorEntities,
+                                ISkillEntity[] skillEntities)
         {
-            var playerStatusVO = playerDataStore.Items.FirstOrDefault();
-            
-            var setSkillVO = skillVODataStore.Items.Where(skillVO => skillVO.Id == 1).ToArray();
-            var skillEntities = skillVODataStore.Items.Select((skillVO, index) => new SkillEntity(index, skillVO)).ToArray();
-            Dictionary<RoleType, ISkillEntity[]> setAllTypeSkillEntities = new Dictionary<RoleType, ISkillEntity[]>();
-            foreach ( var type in Enum.GetValues(typeof(RoleType)))
-            {
-                var roleType = (RoleType)type;
-                if (skillEntities[0].SkillVO.RoleType == roleType)
-                {
-                    setAllTypeSkillEntities.Add(roleType, new ISkillEntity[GameInfo.PLAYER_SET_SKILL_NUM]);
-                    setAllTypeSkillEntities[roleType][0] = skillEntities[0];
-                }
-                else
-                {
-                    setAllTypeSkillEntities.Add(roleType, new ISkillEntity[GameInfo.PLAYER_SET_SKILL_NUM]);
-                }
-            }
-
-            var setWeaponVO = weaponVODataStore.Items.Where(vo => vo.Name != string.Empty).ToArray();
-            _weaponEntities = setWeaponVO.Select((vo, index) => new WeaponEntity(index, vo)).ToArray();
-
-            var setArmorVO = armorVODataStore.Items.Where(vo => vo.Name != string.Empty).ToArray();
-            _armorEntities = setArmorVO.Select((vo, index) => new ArmorEntity(index, vo)).ToArray();
-            player = new SearchPlayerEntity(playerStatusVO, setAllTypeSkillEntities, _weaponEntities.FirstOrDefault(), _armorEntities.FirstOrDefault());
-
             // TODO: ここの処理も別の場所に書く
-            var equipmentActor = new SetEquipmentActor(player, _weaponEntities, _armorEntities);
-            var skillActor = new SetSkillActor(player, skillEntities);
+            var equipmentActor = new SetEquipmentActor((ISetEquipmentEntity)player, weaponEntities, armorEntities);
+            var skillActor = new SetSkillActor((ISetSkillEntity)player, skillEntities);
 
-            Initialize(equipmentActor, skillActor);
-        }
-
-        public void Initialize( ISetEquipmentUseCase equipmentUseCase,
-                                ISetSkillUseCase skillUseCase)
-        {
             _playerMenuVM = new PlayerMenuUIVM();
-            _contentUI.Initialize(_playerMenuVM.State, equipmentUseCase, skillUseCase);
+            _contentUI.Initialize(_playerMenuVM.State, equipmentActor, skillActor);
             _headerUI.UpdateView(_playerMenuVM.State);
         }
 
-        // TODO: デバックのためなので後で消す
         #region InputSystemEventHandler
-        public void InputDebugBattleStartEvent(InputAction.CallbackContext context)
-        {
-            if (!context.performed) return;
-            SceneManager.sceneLoaded += BattleSceneLoaded;
-            SceneManager.LoadScene("Main");
-        }
-
-        // TODO: デバックのためなので後で消す
-        private void BattleSceneLoaded(Scene next, LoadSceneMode mode)
-        {
-            var controller = GameObject.Find("GameController").GetComponent<MainGameController>();
-            controller.Initialize(player);
-            SceneManager.sceneLoaded -= BattleSceneLoaded;
-        }
-
         // TODO: 絶対にUI全体を管理するクラスを作成し、そこで通知を受け取るようにする
         public void InputStickEvent(InputAction.CallbackContext context)
             => _contentUI.InputStickEvent(context);
---
+++ FarmDefenseGame/Assets/Scripts/Search/Controller/SearchGameController.cs
@@ -0,0 +1,76 @@
+using UnityEngine;
+using WolfVillage.MasterDataStore;
+using System.Linq;
+using System.Collections.Generic;
+using WolfVillage.Interface;
+using WolfVillage.Entity.Interface;
+using System;
+using WolfVillage.Entity;
+using UnityEngine.SceneManagement;
+using WolfVillage.Battle;
+using UnityEngine.InputSystem;
+using WolfVillage.Search.PlayerMenuUI;
+
+namespace WolfVillage.Search
+{
+    public class SearchGameController : MonoBehaviour
+    {
+        [SerializeField] private PlayerMenu _playerMenuUI;
+        // TODO: 仮
+        [SerializeField] private WeaponVODataStore weaponVODataStore;
+        [SerializeField] private SkillVODataStore skillVODataStore;
+        [SerializeField] private ArmorVODataStore armorVODataStore;
+        [SerializeField] private PlayerStatusVODataStore playerDataStore;
+
+        private SearchPlayerEntity player;
+        private WeaponEntity[] _weaponEntities;
+        private ArmorEntity[] _armorEntities;
+
+        void Start()
+        {
+            var playerStatusVO = playerDataStore.Items.FirstOrDefault();
+            
+            var setSkillVO = skillVODataStore.Items.Where(skillVO => skillVO.Id == 1).ToArray();
+            var skillEntities = skillVODataStore.Items.Select((skillVO, index) => new SkillEntity(index, skillVO)).ToArray();
+            Dictionary<RoleType, ISkillEntity[]> setAllTypeSkillEntities = new Dictionary<RoleType, ISkillEntity[]>();
+            foreach ( var type in Enum.GetValues(typeof(RoleType)))
+            {
+                var roleType = (RoleType)type;
+                if (skillEntities[0].SkillVO.RoleType == roleType)
+                {
+                    setAllTypeSkillEntities.Add(roleType, new ISkillEntity[GameInfo.PLAYER_SET_SKILL_NUM]);
+                    setAllTypeSkillEntities[roleType][0] = skillEntities[0];
+                }
+                else
+                {
+                    setAllTypeSkillEntities.Add(roleType, new ISkillEntity[GameInfo.PLAYER_SET_SKILL_NUM]);
+                }
+            }
+
+            var setWeaponVO = weaponVODataStore.Items.Where(vo => vo.Name != string.Empty).ToArray();
+            _weaponEntities = setWeaponVO.Select((vo, index) => new WeaponEntity(index, vo)).ToArray();
+
+            var setArmorVO = armorVODataStore.Items.Where(vo => vo.Name != string.Empty).ToArray();
+            _armorEntities = setArmorVO.Select((vo, index) => new ArmorEntity(index, vo)).ToArray();
+
+            player = new SearchPlayerEntity(playerStatusVO, setAllTypeSkillEntities, _weaponEntities.FirstOrDefault(), _armorEntities.FirstOrDefault());
+        }
+
+        #region InputSystemEventHandler
+        // TODO: デバックのためなので後で消す
+        public void InputDebugBattleStartEvent(InputAction.CallbackContext context)
+        {
+            if (!context.performed) return;
+            SceneManager.sceneLoaded += BattleSceneLoaded;
+            SceneManager.LoadScene("Main");
+        }
+        private void BattleSceneLoaded(Scene next, LoadSceneMode mode)
+        {
+            var controller = GameObject.Find("GameController").GetComponent<MainGameController>();
+            controller.Initialize(player);
+            SceneManager.sceneLoaded -= BattleSceneLoaded;
+        }
+        #endregion InputSystemEventHandler
+
+    }
+}
