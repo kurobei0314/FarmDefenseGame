0c66fc6afd868f2b13992a5a4e4de70b423a4829
kurobei0314 <maikurodei.biseibutu@gmail.com>
2025-08-24 15:42:43 +0900

    [fix] searchとPlayerMenuUIのinputの設定を見直し

--- FarmDefenseGame/Assets/Scripts/Search/Controller/MoveController.cs
+++ FarmDefenseGame/Assets/Scripts/Search/Controller/MoveController.cs
@@ -1,10 +1,11 @@
 using UnityEngine.InputSystem;
 using WolfVillage.Search.Interface;
 using UnityEngine;
+using R3;
 
 namespace WolfVillage.Search
 {
-    public class MoveController
+    public class MoveController : MonoBehaviour
     {
         private IMoveUseCase _moveUseCase;
         public MoveController(ISearchCameraView camera)
@@ -12,9 +13,18 @@ namespace WolfVillage.Search
             _moveUseCase = new MoveActor(camera);
         }
 
+        public void Initialize(ISearchCameraView camera)
+        {
+            //Debug.Log("wa-----i");
+            _moveUseCase = new MoveActor(camera);
+            // Observable.EveryUpdate().Where(_ => playerInput.actions["Move"].IsPressed()).Subscribe(_ => {
+            //     //Debug.Log("search wa-----i");
+            // }).AddTo(this);
+        }
+
         public void Input(InputAction.CallbackContext context)
         {
-            if (!context.started) return;
+            if (!context.performed) return;
             var value = context.ReadValue<Vector2>();
             _moveUseCase.Move(value);
         }
